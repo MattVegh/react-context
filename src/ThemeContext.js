4682a4f22a02f1290c4803390a14382380a8ad4f
Matt Vegh <mdv240@gmail.com>
2020-03-10 19:43:04 -0500

    toggleTheme method

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -3,6 +3,19 @@ const { Provider, Consumer } = React.createContext()
 
 class ThemeContextProvider extends Component {
 
+    state = {
+        theme: 'light'
+    }
+
+    toggleTheme = () => {
+        this.setState(
+            prevState => {
+                return {
+                    theme: prevState.theme === 'light' ? 'dark' : 'light'
+                }
+            }
+        )
+    }
     render() {
         return(
         <Provider value={'dark'}>
